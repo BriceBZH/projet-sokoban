eacb4685dcae08b5a0de1e3b3d8f6e2061933b26
BriceBZH <brice.rubeaux@wanadoo.fr>
2024-01-15 21:45:41 +0100

    sutie projet

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 window.addEventListener("DOMContentLoaded", function() {
+
+    const nbCoup = "";
     
     const grid = [
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -33,7 +35,7 @@ window.addEventListener("DOMContentLoaded", function() {
         //4 pour la caisse
         //5 pour la zone de livraison
         for(let r = 0; r < nbRow; r++) {
-            for(let c = 0; c < nbRow; c++) {
+            for(let c = 0; c < nbCol; c++) {
                 let name = ".row"+r+"-col"+c;
                 let div = document.querySelector(name);
                 if(grid[r][c] === 1) {
@@ -61,7 +63,7 @@ window.addEventListener("DOMContentLoaded", function() {
     }
     
     function caisse(grid) {
-        //étant donné que marcel est déjà placé, il ne faudrait pas placer la caisse dessus (ça fait mal)
+        //étant donné que l'alien est déjà placé, il ne faudrait pas placer la caisse dessus (ça fait mal)
         //on vérifie donc si la casse est prise
         let verif = 0;
         while(verif !== 1) {
@@ -78,8 +80,8 @@ window.addEventListener("DOMContentLoaded", function() {
     }
     
     function winBlock(grid) {
-        //étant donné que marcel est déjà placé, il ne faudrait pas placer la caisse dessus (ça fait mal)
-        //pareil pour la caisse sinon aucun interet de joueur si on a gagné dès le début
+        //étant donné que l'alien est déjà placé, il ne faudrait pas placer la caisse dessus (ça fait mal)
+        //pareil pour la caisse sinon aucun interet de jouer si on a gagné dès le début
         //on vérifie donc si la casse est prise
         let verif = 0;
         while(verif !== 1) {
@@ -95,8 +97,10 @@ window.addEventListener("DOMContentLoaded", function() {
         }
     }
     
-    function deplacementMarcel(action, element, grid) {
-        //on regarde où est marcel
+    function deplacementAlien(action, grid, nbCoup) {
+        console.log(nbCoup);
+        let testDeplacement = true;
+        //on regarde où est l'alien
         let x = 0;
         let y = 0;
         for(let r=0;r<10;r++) {
@@ -107,10 +111,15 @@ window.addEventListener("DOMContentLoaded", function() {
                 }
             }
         }
+        let oldX = x;
+        let oldY = y;
         let val = "row"+x+"-col"+y;
         let div = document.querySelector('.'+val);
-        //on enlève son ancien emplacement
+        //on enlève son ancien emplacement (les 4 faces dans le doute)
         div.classList.remove("face");
+        div.classList.remove("back");
+        div.classList.remove("left");
+        div.classList.remove("right");
         grid[x][y] = 2;
         switch(action) {
             case 'ArrowUp':
@@ -118,141 +127,180 @@ window.addEventListener("DOMContentLoaded", function() {
                 if(x < 1) {
                     x = 1;
                 }
-                val = "row"+x+"-col"+y;
-                div = document.querySelector('.'+val);
-                //nouvel emplcament
-                div.classList.add("face");
-                deplacementCaisse(x, y, grid, action);
-                grid[x][y] = 3;
+                testDeplacement = deplacementCaisse(x, y, grid, action);
+                if(!testDeplacement) { // erreur retour à l'emplacement précédent
+                    val = "row"+oldX+"-col"+oldY;
+                    div = document.querySelector('.'+val);
+                    div.classList.add("face");
+                    grid[oldX][oldY] = 3;
+                } else { // c'est ok on met le nouvel emplacement
+                    val = "row"+x+"-col"+y;
+                    div = document.querySelector('.'+val);
+                    div.classList.add("back");
+                    grid[x][y] = 3;
+                    nbCoup = 5;
+                }
                 break;
             case 'ArrowDown':
                 x += 1
                 if(x > 8) {
                     x = 8;
+                }     
+                testDeplacement = deplacementCaisse(x, y, grid, action);
+                if(!testDeplacement) { // erreur retour à l'emplacement précédent
+                    val = "row"+oldX+"-col"+oldY;
+                    div = document.querySelector('.'+val);
+                    div.classList.add("face");
+                    grid[oldX][oldY] = 3;
+                } else { // c'est ok on met le nouvel emplacement
+                    val = "row"+x+"-col"+y;
+                    div = document.querySelector('.'+val);
+                    div.classList.add("face");
+                    grid[x][y] = 3;
+                    nbCoup = 5;
                 }
-                val = "row"+x+"-col"+y;
-                div = document.querySelector('.'+val);
-                //nouvel emplcament
-                div.classList.add("face");
-                deplacementCaisse(x, y, grid, action);
-                grid[x][y] = 3;
                 break;
             case 'ArrowLeft':
                 y -= 1
                 if(y < 1) {
                     y = 1;
                 }
-                val = "row"+x+"-col"+y;
-                div = document.querySelector('.'+val);
-                //nouvel emplcament
-                div.classList.add("face");
-                deplacementCaisse(x, y, grid, action);
-                grid[x][y] = 3;
+                testDeplacement = deplacementCaisse(x, y, grid, action);
+                if(!testDeplacement) { // erreur retour à l'emplacement précédent
+                    val = "row"+oldX+"-col"+oldY;
+                    div = document.querySelector('.'+val);
+                    div.classList.add("face");
+                    grid[oldX][oldY] = 3;
+                } else { // c'est ok on met le nouvel emplacement
+                    val = "row"+x+"-col"+y;
+                    div = document.querySelector('.'+val);
+                    div.classList.add("left");
+                    grid[x][y] = 3;
+                    nbCoup = 5;
+                }
                 break;
             case 'ArrowRight':
                 y += 1
                 if(y > 8) {
                     y = 8;
                 }
-                val = "row"+x+"-col"+y;
-                div = document.querySelector('.'+val);
-                //nouvel emplcament
-                div.classList.add("face");
-                deplacementCaisse(x, y, grid, action);
-                grid[x][y] = 3;
+                testDeplacement = deplacementCaisse(x, y, grid, action);
+                if(!testDeplacement) { // erreur retour à l'emplacement précédent
+                    val = "row"+oldX+"-col"+oldY;
+                    div = document.querySelector('.'+val);
+                    div.classList.add("face");
+                    grid[oldX][oldY] = 3;
+                } else { // c'est ok on met le nouvel emplacement
+                    val = "row"+x+"-col"+y;
+                    div = document.querySelector('.'+val);
+                    div.classList.add("right");
+                    grid[x][y] = 3;
+                    nbCoup = 5;
+                }
                 break;
         }
+        console.log(nbCoup);
+        nbCoup = 6;
+        return nbCoup;
     }
     
-    function deplacementCaisse(xMarcel, yMarcel, grid, direction) {
+    function deplacementCaisse(xAlien, yAlien, grid, direction) {
         switch(direction) {
             case 'ArrowUp':
-                if(grid[xMarcel][yMarcel] === 4) { //marcel arrive sur la caisse donc on déplace la caisse
-                    let x = xMarcel - 1;
+                if(grid[xAlien][yAlien] === 4) { //l'alien arrive sur la caisse donc on déplace la caisse
+                    let x = xAlien - 1;
                     if(x < 1) {
                         x = 1;
+                        return false;
                     }
                     //on enlève ancienne position
-                    let val = "row"+xMarcel+"-col"+yMarcel;
+                    let val = "row"+xAlien+"-col"+yAlien;
                     let div = document.querySelector('.'+val);
                     div.classList.remove("box");
                     //on ajoute new position
-                    val = "row"+x+"-col"+yMarcel;
+                    val = "row"+x+"-col"+yAlien;
                     div = document.querySelector('.'+val);
                     div.classList.add("box");
-                    victoire(x, yMarcel, grid);
-                    grid[x][yMarcel] = 4;
+                    victoire(x, yAlien, grid);
+                    grid[x][yAlien] = 4;
+                    //si on ne peut pas déplacer la caisse on retourne une erreur pour ne pas que l'alien chevauche la caisse
                 }
                 break;
             case 'ArrowDown': 
-                if(grid[xMarcel][yMarcel] === 4) { //marcel arrive sur la caisse donc on déplace la caisse
-                    let x = xMarcel + 1;
+                if(grid[xAlien][yAlien] === 4) { //l'alien arrive sur la caisse donc on déplace la caisse
+                    let x = xAlien + 1;
                     if(x > 8) {
                         x = 8;
+                        return false;
                     }
                     //on enlève ancienne position
-                    let val = "row"+xMarcel+"-col"+yMarcel;
+                    let val = "row"+xAlien+"-col"+yAlien;
                     let div = document.querySelector('.'+val);
                     div.classList.remove("box");
                     //on ajoute new position
-                    val = "row"+x+"-col"+yMarcel;
+                    val = "row"+x+"-col"+yAlien;
                     div = document.querySelector('.'+val);
                     div.classList.add("box");
-                    victoire(x, yMarcel, grid);
-                    grid[x][yMarcel] = 4;
+                    victoire(x, yAlien, grid);
+                    grid[x][yAlien] = 4;
                 }
                 break;
             case 'ArrowLeft':
-                if(grid[xMarcel][yMarcel] === 4) { //marcel arrive sur la caisse donc on déplace la caisse
-                    let y = yMarcel - 1;
+                if(grid[xAlien][yAlien] === 4) { //l'alien arrive sur la caisse donc on déplace la caisse
+                    let y = yAlien - 1;
                     if(y < 1) {
                         y = 1;
+                        return false;
                     }
                     //on enlève ancienne position
-                    let val = "row"+xMarcel+"-col"+yMarcel;
+                    let val = "row"+xAlien+"-col"+yAlien;
                     let div = document.querySelector('.'+val);
                     div.classList.remove("box");
                     //on ajoute new position
-                    val = "row"+xMarcel+"-col"+y;
+                    val = "row"+xAlien+"-col"+y;
                     div = document.querySelector('.'+val);
                     div.classList.add("box");
-                    victoire(xMarcel, y, grid);
-                    grid[xMarcel][y] = 4;
+                    victoire(xAlien, y, grid);
+                    grid[xAlien][y] = 4;
                 }
                 break;
             case 'ArrowRight':
-                if(grid[xMarcel][yMarcel] === 4) { //marcel arrive sur la caisse donc on déplace la caisse
-                    let y = yMarcel + 1;
+                if(grid[xAlien][yAlien] === 4) { //l'alien arrive sur la caisse donc on déplace la caisse
+                    let y = yAlien + 1;
                     if(y > 8) {
                         y = 8;
+                        return false;
                     }
                     //on enlève ancienne position
-                    let val = "row"+xMarcel+"-col"+yMarcel;
+                    let val = "row"+xAlien+"-col"+yAlien;
                     let div = document.querySelector('.'+val);
                     div.classList.remove("box");
                     //on ajoute new position
-                    val = "row"+xMarcel+"-col"+y;
+                    val = "row"+xAlien+"-col"+y;
                     div = document.querySelector('.'+val);
                     div.classList.add("box");
-                    victoire(xMarcel, y, grid);
-                    grid[xMarcel][y] = 4;
+                    victoire(xAlien, y, grid);
+                    grid[xAlien][y] = 4;
                 }
                 break;
         }
+        return true;
     }
     
     function victoire (x, y, grid) {
-        console.log("ici-"+grid[x][y]+"-"+x+"-"+y);
         if(grid[x][y] === 5) {
-            console.log("victoire");
+            console.log("Victoire");
             alert("Vous avez gagné");   
         }
     }
+
+    //function restart si partie gagnée
     
     
     plateau(10, 10);
     createDecor(10, 10, grid);
-    marcel.dom = document.querySelector('.face');
-    console.log(grid);
-    document.addEventListener('keydown', (e) => deplacementMarcel(e.key, marcel, grid));
+    document.addEventListener( 'keydown', (e) => deplacementAlien(e.key, grid, nbCoup));
+    
+    //quand on gagne ne pas pouvoir continuer la partie
+    //compter le nombre de coup pour gagner
 });
